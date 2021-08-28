bff7d533534820073eb7d0de2b1bdf7cd7813a0f
“nkusikevin” <nkusikvnhart@gmail.com>
2021-08-28 03:54:48 +0200

    final commit

--- src/components/containers/HistoryRates.jsx
+++ src/components/containers/HistoryRates.jsx
@@ -3,10 +3,10 @@ import moment from "moment";
 import DatePicker from "react-datepicker";
 import axios from "axios";
 import "react-datepicker/dist/react-datepicker.css";
-import { Card } from "../styles";
+// import { Card } from "../styles";
 function HistoryRates() {
 	const [selectedDate, setSelectedDate] = useState(null);
-	const [data, setdata] = useState();
+	const [hData ,setHdata] = useState(null);
 	var today = new Date();
 	var date = "";
 	if (selectedDate === null) {
@@ -14,21 +14,20 @@ function HistoryRates() {
 	} else {
 		date = moment(selectedDate).format("yyyy-MM-DD");
 	}
-	// const fetchData = async () => {};
-	useEffect(() => {
-		axios
-			.get(
-				`http://data.fixer.io/api/${date}?access_key=7be9ecd39c520e2cd5c5fad627913de2& base = GBP& symbols = USD,CAD,EUR`
-			)
-			.then((results) => {
-				setdata(results.data.rates);
-				console.log(results);
-				console.log("date:"+date);
-			});
-	}, [date]);
-
-	console.log(selectedDate);
-	console.log(date);
+	// eslint-disable-next-line
+	const fetchData = async (date) => {
+		const results = await axios.get(
+			`http://data.fixer.io/api/${date}?access_key=7be9ecd39c520e2cd5c5fad627913de2& base = GBP& symbols = USD,CAD,EUR`
+			);
+			console.log(results.data);
+			const datas = JSON.stringify(results.data.rates);
+			
+			setHdata(datas);
+			
+		};
+		useEffect(() => {
+			fetchData(date);
+		}, [date]);
 
 	return (
 		<>
@@ -38,17 +37,17 @@ function HistoryRates() {
 					onChange={(date) => setSelectedDate(date)}
 					dateFromat='YYYY-MM-dd'
 					placeholderText={"yyyy-mm-dd"}
-					// minDate={new Date()}
-					// maxDate={new Date()}
 					filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
 					showYearDropdown
 					scrollableYearDropdown
 				/>
 			</div>
-			{/* {Object.keys(data).map((key) => (
-				<Card>
-					{key} - {data[key]}
-				</Card>
+			{hData ? undefined : <h1>Unable to fetch data now</h1>}
+		
+			{/* {Object.keys(hData).map((key) => (
+			<Card>
+				{key} - {hData[key]}
+			</Card>
 			))} */}
 		</>
 	);
